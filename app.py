a0a622ef8f91dc2d2979414260239d3f4f09894a
Michael Herman <hermanmu@gmail.com>
2013-11-01 14:45:15 -0600

    while loop

--- app.py
+++ app.py
@@ -5,17 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return loop(100)
+    return 'Hi World!'
 
-def loop(num):
-	x = 1
-	array = []
-	for x in range(0,n):
-	    array.append(x)
-	    x +=1
-	return array
+@app.route('/loop', defaults={"a":0, "b":100})
+
+def endless_loop(a, b):
+    count = a
+    array = []
+    while (count < b):
+       print 'The count is:', count
+       array.append(count)
+       count += 1
+    print "Good bye!"
+
 
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT',5001))
   app.run(host='0.0.0.0', port=port)
+
+
+
