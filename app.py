1711b5e11e46fbb4cca115af74559e63ea874093
Michael Herman <hermanmu@gmail.com>
2013-11-01 14:30:19 -0600

    loop

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hi World!'
+    return loop(100)
+
+def loop(num):
+	x = 1
+	array = []
+	for x in range(0,n):
+	    array.append(x)
+	    x +=1
+	return array
+
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT',5000))
+  port = int(os.environ.get('PORT',5001))
   app.run(host='0.0.0.0', port=port)
